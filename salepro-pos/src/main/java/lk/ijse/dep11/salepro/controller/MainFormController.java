a55804467c5f67cb26419a4563bec6089e7cfa73
isuranga <deshikaisu95@gmail.com>
2024-02-08 10:43:23 +0530

    fx part on homepage

---
+++ salepro-pos/src/main/java/lk/ijse/dep11/salepro/controller/MainFormController.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep11.salepro.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane root;
+    public ImageView imgCustomer;
+    public ImageView imgItem;
+    public ImageView imgOrder;
+    public Label lblMenu;
+    public Label lblDescription;
+
+    public void navigate(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseEnterAnimation(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseExitAnimation(MouseEvent mouseEvent) {
+    }
+}
